d771ef0e9318848cf908f9efd4d7c067fd9c7403
Carlos Z. Bent <bentsoft365@gmail.com>
2022-08-30 20:39:02 -0400

    add animation to subscribe button when email field is interacted with, change subscribe button animation duration to .5 seconds

--- script.js
+++ script.js
@@ -12,28 +12,43 @@ let currentWorkPage = document.getElementById('currentWorkPage');
 let followMePage = document.getElementById('followMePage');
 let contactMePage = document.getElementById('contactMePage');
 
+let subscribeEmail = document.getElementById('member_email');
 let subscribeButton = document.getElementById('member_submit');
 
 const showOverlay = function (page, container) {
     page.style.display = 'grid';
     container.style.display = 'block';
-  }
+  };
 
 document.onclick = function(event){
   let target = event.target;
     if(target.className == 'overlay') {
       target.style.display = 'none';
     } 
-  }
+  };
 
 const closeOverlay = function (page, container) {
     page.style.display = 'none';
     container.style.display = 'none';
-}
+};
 
+// animate button on hover
 subscribeButton.onmouseover = () => {
   subscribeButton.classList += ' vibrate-1';
   subscribeButton.addEventListener('animationend', () => {
     subscribeButton.className = 'revue-subscribe-button';
   })
-}
+};
+
+// when subscribe button is clicked its animation ends
+subscribeButton.onclick = () => {
+  subscribeButton.classList = 'revue-subscribe-button';
+};
+
+// animate subscribe button when email field is interacted with
+subscribeEmail.oninput = () => {
+  subscribeButton.classList += ' vibrate-1';
+  subscribeButton.addEventListener('animationend', () => {
+    subscribeButton.className = 'revue-subscribe-button';
+  })
+};
